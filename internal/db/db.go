e982aec6372dde77d392a48f40e900fc3be98b28
ratheeshkumar25 <ratheeshgopinadhkumar@gmail.com>
2025-01-25 00:52:49 +0530

    db file updated

--- internal/db/db.go
+++ internal/db/db.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ratheeshkumar25/blog-crud-api/internal/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func ConnectDB(config *config.Config) *gorm.DB {
@@ -16,18 +17,20 @@ func ConnectDB(config *config.Config) *gorm.DB {
 	// Log the connection details
 	log.Printf("Connecting to DB with URL: %s", config.Database_url)
 
-	DB, err := gorm.Open(postgres.Open(config.Database_url), &gorm.Config{})
-	if err != nil {
-		log.Fatal("connection to the database failed:", err)
-	}
-
-	err = DB.AutoMigrate(
-		&model.BlogPost{},
-	)
+	DB, err := gorm.Open(postgres.Open(config.Database_url), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 
 	if err != nil {
-		log.Printf("error while migrating %v", err.Error())
+		log.Printf("Error while connecting to the database: %v", err)
 		return nil
 	}
+
+	// Migrate the schema
+	if err := DB.AutoMigrate(&model.BlogPost{}); err != nil {
+		log.Printf("Error while migrating: %v", err)
+		return nil
+	}
+
 	return DB
 }
